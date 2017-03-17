00f67886879dad130fe1cb8bbc45559c08bd30c3
andy <andymaheshw@gmail.com>
2017-03-16 22:20:28 -0400

    update for today

--- func1-15.c
+++ func1-15.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 
 /* print Fahrenheit-Celsius table for fahr = 0, 20, 300 */
+int power(int fahr, int step, int upper);
 
-main()
+int main()
 {
     float fahr, celsius;
     int lower, upper, step;
 
+    fahr = 0;
     lower = 0;
     upper = 300;
     step = 20;
 
     fahr = lower;
-    while (fahr <= upper) {
+    return power(fahr, step, upper);
+}
+
+int power(int fahr, int step, int upper)
+{
+
+    double celsius;
+    celsius = 0;
+        while (fahr <= upper) {
         celsius = (5.0/9.0) * (fahr - 32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
+        printf("%d %3.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
-
+    return 0;
 }
+
