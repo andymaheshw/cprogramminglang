b8211eae353c900c843639c7ce7e09ed76a79f6c
andy <andymaheshw@gmail.com>
2017-04-20 23:03:24 -0400

    4-1

--- 4-2.c
+++ 4-2.c
@@ -39,32 +39,3 @@ int getline(char s[], int lim)
         return i;
     }
 }
-
-int strindex(char s[], char t[])
-{
-    int i, j, k;
-
-    for(i = 0; s[i] != '\0'; i++)
-    {
-        for(j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++)
-            ;
-        if (k > 0 && t[k] == '\0')
-            return i;
-    }
-    return -1;
-}
-
-
-int strindexr(char s[], char t[])
-{
-    int i, j, k;
-
-    for(i = strlen(s); i != 0; i--)
-    {
-        for(j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++)
-            ;
-        if (k > 0 && t[k] == '\0')
-            return i;
-    }
-    return -1;
-}
